a6e8ed1e094becba32ca03c4ae91283eb2fb367c
Makhdoom Shabir <MShabir@qa.com>
2020-08-26 16:14:19 +0100

    created NotesController  file

--- src/main/java/com/qa/notes/rest/NotesController.java
+++ src/main/java/com/qa/notes/rest/NotesController.java
@@ -1,4 +1,46 @@
 package com.qa.notes.rest;
 
+import com.qa.notes.domain.Note;
+import com.qa.notes.service.NoteService;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class NotesController {
+
+    private final NoteService noteService;
+
+    @Autowired
+    public NotesController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @GetMapping("/")
+    public List<Note> getAllNotes(){
+        return this.noteService.readAllNotes();
+    }
+
+    @PostMapping("/createNote")
+    public Note createNote(@RequestBody Note note){
+        return this.noteService.createNote(note);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Boolean deleteNote(@PathVariable Long id){
+        return this.noteService.deleteNoteById(id);
+    }
+
+    @GetMapping("/getNoteById/{id}")
+    public Note getNoteById(@PathVariable Long id){
+        return this.noteService.findNoteById(id);
+    }
+
+    @PutMapping("/updateNote/{id}")
+    public Note updateNote(@PathVariable Long id, @RequestBody Note note){
+        return this.noteService.updateNote(id, note);
+    }
+
 }
