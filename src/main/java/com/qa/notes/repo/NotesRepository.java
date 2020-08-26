70b67d6b3a034a6216f2c6c57ea1665c90819091
Makhdoom Shabir <MShabir@qa.com>
2020-08-26 16:12:31 +0100

    created NoteRepository interface file

--- src/main/java/com/qa/notes/repo/NotesRepository.java
+++ src/main/java/com/qa/notes/repo/NotesRepository.java
@@ -1,4 +1,8 @@
 package com.qa.notes.repo;
+import com.qa.notes.domain.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface NotesRepository {
+@Repository
+public interface NotesRepository extends JpaRepository<Note, Long>{
 }
